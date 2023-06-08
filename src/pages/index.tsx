35ca164a184b25bba6b7812942b5bb3dc528077a
Mohamed-bip <97035858+Mohamed-bip@users.noreply.github.com>
2023-06-08 17:40:56 +0200

    Add files via upload

---
+++ src/pages/index.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import Head from 'next/head';
+import { Input } from '../components/input';
+import { useHistory } from '../hooks/history';
+import { History } from '../components/history';
+import { NextPageContext } from 'next';
+import packageJson from '../../package.json';
+import { getQuote } from '../api';
+import { shell } from '../utils/shell';
+
+const IndexPage: React.FC<{ version: string; quote: string }> = ({
+  version,
+  quote,
+}) => {
+  const inputRef = React.useRef(null);
+  const containerRef = React.useRef(null);
+  const { history, command, setCommand, setHistory, clearHistory } = useHistory(
+    [],
+  );
+
+  const init = React.useCallback(
+    () =>
+      setHistory(
+        `
+    ________  ____    __   _ _____ __  ______  ____ 
+   / ____/ / / / /   / /  ( ) ___// / / / __ \/ __ \
+  / /_  / / / / /   / /   |/\__ \/ /_/ / / / / /_/ /
+ / __/ / /_/ / /___/ /___  ___/ / __  / /_/ / ____/ 
+/_/    \____/_____/_____/ /____/_/ /_/\____/_/      
+
+Type 'help' to see list of available commands.
+--
+For a simplified version, click <a class="text-gruvboxlight-blue dark:text-gruvboxdark-blue underline" href="/gui">Here</a>.
+--
+${quote}
+`,
+      ),
+    [quote, version],
+  );
+
+  React.useEffect(() => {
+    init();
+  }, [init]);
+
+  React.useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [history]);
+
+  const onSubmit = async (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' || event.code === '13') {
+      await shell(history, command, setHistory, clearHistory, setCommand);
+
+      containerRef.current.scrollTo(0, containerRef.current.scrollHeight);
+    }
+  };
+
+  const onChange = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setCommand(value);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>FULL'SHOP | ZEYKOX</title>
+      </Head>
+
+      <div className="p-8 overflow-hidden h-full border-2 rounded border-gruvboxlight-yellow dark:border-gruvboxdark-yellow">
+        <div ref={containerRef} className="overflow-y-auto h-full">
+          <History history={history} />
+
+          <Input
+            inputRef={inputRef}
+            command={command}
+            onChange={onChange}
+            onSubmit={onSubmit}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export async function getStaticProps(context: NextPageContext) {
+  const { quote } = await getQuote();
+  return {
+    props: {
+      version: packageJson.version,
+      quote,
+    },
+    revalidate: true,
+  };
+}
+
+export default IndexPage;
---
+++ src/utils/bin/social.ts
@@ -0,0 +1,14 @@
+export const instagram = async (args: string[]): Promise<string> => {
+  window.open('https://www.instagram.com/zeykox/');
+  return 'Opening instagram...';
+};
+
+export const github = async (args: string[]): Promise<string> => {
+  window.open('https://github.com/m4tt72/');
+  return 'Opening github...';
+};
+
+export const linkedin = async (args: string[]): Promise<string> => {
+  window.open('https://dsc.gg/full-shop');
+  return 'Opening Discord...';
+};
